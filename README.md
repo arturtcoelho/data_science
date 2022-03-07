1ad74714b2fbf879d04c06804f7a4c6455deaad0
Artur Coelho <artur.temporal@hotmail.com>
2022-03-07 15:46:56 -0300

    analise inicial de atributos

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
 # data_science
+## O Dataset
+O dataset utilizado foi o IOT dataset do laboratorio estratosferico de Praga
+https://www.stratosphereips.org/datasets-iot23
+Esse dataset consiste de trafico de rede em equipamentos de IOT,
+capturados entre 2018 e 2019, sendo eles rotulados de acordo com o tipo de ataque
 
+Ele esta organizado em 20 capturas com conexões maliciosas e 3 capturas puramente benignas
+
+No total são 106Gb de dados, e foram utilizadas apenas algumas capturas por vez para cada analise
+
+### dataset.py
+Contem os nomes de arquivos do dataset
+
+### explore.py
+Faz a leitura dos arquivos de log do dataset e realiza algumas analises
+
+### parse_pcap.py
 O arquivo conta_sessoes.py lê um arquivo .pcap, com as informações lidas da rede, e conta o numero de sessões e seus tipos
--- explore.py
+++ explore.py
@@ -1,11 +1,11 @@
 import re
+import matplotlib as mpl
 
-from attr import fields
 import dataset
 import traceback
 
-base_path = 'opt/Malware-Project/BigDataset/IoTScenarios/'
-filepath_complement = '/bro/conn.log.labeled'
+# base_path = 'opt/Malware-Project/BigDataset/IoTScenarios/'
+# filepath_complement = '/bro/conn.log.labeled'
 
 fields_map = None
 dataset_dict = {}
@@ -23,25 +23,49 @@ def generate_fields_map(line):
 
 def parse_line(file, line):
     if file not in dataset_dict:
-        dataset_dict[file] = []
-
+        dataset_dict[file] = {}
+        
     if line[0][0] == '#':
         if '#fields' in line:
             generate_fields_map(line)
-        return
+            for _, field in fields_map.items():
+                dataset_dict[file][field] = {}
+        return    
     
-    dataset_dict[file].append({fields_map[i]:line[i] for i, field in enumerate(line)})
-    # do some stuff with this
+    for i, field in enumerate(line):
+        if field not in dataset_dict[file][fields_map[i]]:
+            dataset_dict[file][fields_map[i]][field] = 1
+        else:
+            dataset_dict[file][fields_map[i]][field] += 1
     
+def parse_dict(dataset_dict, file):
+    for item in dataset_dict[file]:
+        print(item)
+
+def analisys(atribute):
+    total = sum([int(i[1]) for i in dataset_dict[datafile][atribute].items()])
+    print('IPs unicos:', len(list(dataset_dict[datafile][atribute].items())))
+    top_ip = max(list(filter(lambda k: k[1] > 5, list(dataset_dict[datafile][atribute].items()))), key=lambda k: k[1])
+    print(f'IP {top_ip[0]} representa {round((top_ip[1]/total)*100, 2)}% das chamadas')
+
 if __name__ == '__main__':
     try:
 
-        for datafile in dataset.filenames: # For each dataset file
+        datafile = 'test'
+        # for datafile in dataset.filenames: # For each dataset file
 
-            with open(base_path+datafile+filepath_complement, 'r') as file:
-                for line in file: # for each line
-                    parse_line(datafile, line.split())
+        # with open(base_path+datafile+filepath_complement, 'r') as file:
+        with open('conn.log.labeled', 'r') as file:
+            for line in file: # for each line
+                parse_line(datafile, line.split())
 
-            print('FILE:\t', datafile, len(dataset_dict[datafile]))
+        # parse_dict(dataset_dict, datafile)
+        print('Os atributos do dataset:')
+        for k in fields_map.keys():
+            print(f"{fields_map[k]}")
+        print()
+        print(f'Captura: \t {datafile}')
+        analisys('id.orig_h')
+        analisys('id.resp_h')
     except Exception as e_:
         traceback.print_exc()
