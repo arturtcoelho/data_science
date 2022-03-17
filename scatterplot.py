960db70e1dbc0c994f361900732440aee7ab35c1
atc19 <atc19@inf.ufpr.br>
2022-03-17 14:30:35 -0300

    run many scatterplots

--- scatterplot.py
+++ scatterplot.py
@@ -13,37 +13,46 @@ import pandas as pd
 import dataset
 import traceback
 
+enum_dict = {}
+
+def number_map(col, row):
+    try:
+        return float(row)
+    except ValueError:
+        return 0
+
+def enum_map(col, row):
+    try:
+        return enum_dict[col][row]
+    except KeyError:
+        return -1
+
+
 fields=[
 #'ts',
 # 'uid',
 # 'id.orig_h',
 # 'id.orig_p',
-# 'id.resp_h',
-# 'id.resp_p',
-# 'proto',
-# 'service',
-'duration',
-# 'orig_bytes',
-# 'resp_bytes',
-# 'conn_state',
-# 'local_orig',
-# 'local_resp',
-# 'missed_byteshistory',
-# 'orig_pkts',
-# 'orig_ip_bytes',
-'resp_pkts',
-# 'resp_ip_bytes',
-# 'tunnel_parents',
+# ('id.resp_h', enum_map),
+('id.resp_p', enum_map),
+# ('proto', enum_map),
+# ('service', enum_map),
+('duration', number_map),
+('orig_bytes', number_map),
+('resp_bytes', enum_map),
+('conn_state', enum_map),
+('local_orig', enum_map),
+('local_resp', enum_map),
+('missed_bytes', enum_map),
+('history', enum_map),
+('orig_pkts', number_map),
+('orig_ip_bytes', number_map),
+('resp_pkts', number_map),
+('resp_ip_bytes', number_map),
+('tunnel_parents', enum_map),
 # 'label',
 # 'detailed-label'
 ]
-
-def map_val(val):
-    try:
-        return float(val)
-    except ValueError:
-        return 0
-
 
 if __name__ == '__main__':
     '''
@@ -52,21 +61,55 @@ if __name__ == '__main__':
     try:
         datafile = 'conn.log.labeled'
         df = pd.read_csv('conn.log.labeled', delim_whitespace=True, low_memory=False)
+
+        # make enum dict of enum fields
+        for col in df:
+            if str(col) not in [f[0] for f in fields]:
+                continue
+
+            for f in fields:
+                if f[0] == col and f[1] != enum_map:
+                    continue
+
+            enum_dict[col] = {}
+
+            count = 0
+            for row in df[col]:
+                enum_dict[col][row] = count
+                count += 1
+
         mal_df = df.loc[df['label'] == 'Malicious']
         beg_df = df.loc[df['label'] == 'Benign']
 
-        # for i in fields:
-        #     for j in fields:
-        i = fields[0]
-        j = fields[1]
-        print(i)
-        print(j)
-        plt.scatter(mal_df.applymap(map_val)[i], mal_df.applymap(map_val)[j], c='red')
-        plt.scatter(beg_df.applymap(map_val)[i], beg_df.applymap(map_val)[j], c='blue')
-        plt.xlabel(i)
-        plt.ylabel(j)
-        # plt.savefig(f'./images/{i}_{j}.png')
-        plt.show()
+        for field1, func1 in fields:
+            for field2, func2 in fields:
+                try:
+
+                    if field1==field2:
+                        continue
+                    if field1 not in [f[0] for f in fields] or field2 not in [f[0] for f in fields]:
+                        continue
+
+                    print(field1)
+                    print(field2)
+
+                    def map_func(col, row, func):
+                        return func(col, row)
+
+                    plt.scatter(beg_df.applymap(lambda k: map_func(field1, k, func1))[field1], 
+                                beg_df.applymap(lambda k: map_func(field2, k, func2))[field2], c='blue')
+
+                    plt.scatter(mal_df.applymap(lambda k: map_func(field1, k, func1))[field1], 
+                                mal_df.applymap(lambda k: map_func(field2, k, func2))[field2], c='red')
+
+                    plt.xlabel(field1)
+                    plt.ylabel(field2)
+                    plt.savefig(f'./images/{field1}-{field2}.png')
+
+                except Exception as e_:
+                    traceback.print_exc()
+                    print('error on', field1, field2)
+                    continue
 
     except Exception as e_:
         traceback.print_exc()
