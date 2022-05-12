da50c6d075f89f05aa812286546b1d0fa9577ece
Artur Coelho <artur.temporal@hotmail.com>
2022-05-12 13:14:41 -0300

    add parser

--- filter_ddos.py
+++ filter_ddos.py
@@ -1,14 +1,8 @@
-with open('shuf_60.csv', 'r') as f:
+with open('parsed_files/CTU-IoT-Malware-Capture-1-1.csv', 'r') as f:
     l = f.readlines()
 
-
-l = [x if x.split(',')[-1] != '\n' else x[:-1] + 'Benign\n' for x in l if x.split(',')[-1] != 'DDoS\n']
+l = [x if x.split(',')[-1] != '\n' else x[:-1] + 'Benign\n' for x in l if x.split(',')[-1] != 'DDoS\n' and x.split(',')[-1] != 'PartOfAHorizontalPortScan\n']
 l = [x for x in l if x.split(',')[-1] != 'DDoS\n']
 
-size = len(l)
-
-with open('parsed_60_80.csv', 'w') as f:
-    f.write(''.join(l[:int(size*0.8)]))
-
-with open('parsed_60_20.csv', 'w') as f:
-    f.write(''.join(l[int(size*0.8):]))
+with open('parsed_1.csv', 'w') as f:
+    f.write(''.join(l))
